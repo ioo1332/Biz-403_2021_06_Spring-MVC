e00709e2b71cf10835eb286ffe1a882f1fd70024
ioo1332 <ioo168400@gmail.com>
2021-06-28 15:18:11 +0900

    secret

---
+++ SpMVC_040_book/src/main/java/com/callor/book/config/NaverSecret_Sample.java
@@ -0,0 +1,12 @@
+package com.callor.book.config;
+
+public class NaverSecret_Sample {
+	
+	/*
+	 *  네이버 개발자 센터에 어플을 등록한후
+	 *  CLIENT_ID CLIENT_SECRET을 발급받아
+	 *  다음 항목에 작성한후 파일이름을 NaverSecret으로 변경한후 프로젝트를 실행하라
+	 */
+	public static final String NAVER_CLIENT_ID="YOUR NAVER CLIENT ID";
+	public static final String NAVER_CLIENT_SECTRET="YOUR NAVER SECRETE";
+}
